fa1fe8c2b63c5c1c2f148ba85a8e868346f08e9a
xingzhewj <wangjie0806@qq.com>
2014-08-17 22:16:49 +0800

    0817

--- public/javascripts/interface.js
+++ public/javascripts/interface.js
@@ -1,1 +1,2 @@
 天气接口: http://cdn.weather.hao.360.cn/sed_api_weather_info.php?code=101010100&v=2&app=hao360cn&_jsonp=__jsonp4__&t=2345923
+博客园博客接口：http://wcf.open.cnblogs.com/blog/sitehome/paged/1/10
